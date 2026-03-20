0e266bb570594f981a0f74867e6bf26318b6bb59
Ethan Omessi <ethanomessi@gmail.com>
2026-03-20 11:33:57 -0500

    Client category done

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
 import { Authorized } from "./Authorized"
+import CreateCategory from "../components/category/createCategory"
 
 export const ApplicationViews = ({ token, setToken }) => {
   return <>
@@ -9,8 +10,8 @@ export const ApplicationViews = ({ token, setToken }) => {
       <Route path="/login" element={<Login setToken={setToken} />}  />
       <Route path="/register" element={<Register setToken={setToken} />}  />
       <Route element={<Authorized token={token} />}>
-        {/* Add Routes here */}
-        
+      <Route path="create-category" element={<CreateCategory token={token}/>}/>
+      
       </Route>
     </Routes>
   </>
