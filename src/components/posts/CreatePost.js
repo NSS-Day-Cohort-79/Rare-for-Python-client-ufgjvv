ece5f8c5d4a56437ccd510c559a611537ffa2b21
Jack Gardner <jtgardner99@gmail.com>
2026-03-23 10:58:15 -0500

    added window alert

--- src/components/posts/CreatePost.js
+++ src/components/posts/CreatePost.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { getCategories } from "../../managers/categoryManager"
 import { getTags } from "../../managers/tagManager"
 import { postPost } from "../../managers/postManager"
+import { useNavigate } from "react-router-dom"
 
 export const CreatePost = ({ token }) => {
     const [newPost, setNewPost] = useState({
@@ -17,24 +18,32 @@ export const CreatePost = ({ token }) => {
     const [tag, setTag] = useState([])
     const [category, setCategory] = useState(0)
 
+    const navigate = useNavigate()
+
     useEffect(() => {
         getCategories().then(setCategories)
         getTags().then(setTags)
     }, [token])
 
     const handlePublish = () => {
-        const post = {
-            user_id: token,
-            category_id: category,
-            title: newPost.title,
-            publication_date: new Date(),
-            image_url: newPost.image_url,
-            content: newPost.content,
-            approved: newPost.approved,
-            tags: tag
+        if (category && newPost.title && newPost.image_url && newPost.content && tag) {
+            const post = {
+                user_id: token,
+                category_id: category,
+                title: newPost.title,
+                publication_date: new Date(),
+                image_url: newPost.image_url,
+                content: newPost.content,
+                approved: newPost.approved,
+                tags: tag
+            }
+    
+            postPost(post).then(() => {
+                navigate("/")
+            })
+        } else {
+            window.alert("Please make sure all fields are filled out before publishing")
         }
-
-        postPost(post)
     }
 
     return (
