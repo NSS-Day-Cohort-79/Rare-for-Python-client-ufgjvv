274898e6641f04cf35983d48d1c1aa546eff1651
Evan Alford <evan.al4d@gmail.com>
2026-03-24 11:40:10 -0500

    added path

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
 import { Authorized } from "./Authorized"
+import { CategoryList } from "../components/categories/CategoryList"
 
 export const ApplicationViews = ({ token, setToken }) => {
   return <>
@@ -9,6 +10,7 @@ export const ApplicationViews = ({ token, setToken }) => {
       <Route path="/login" element={<Login setToken={setToken} />}  />
       <Route path="/register" element={<Register setToken={setToken} />}  />
       <Route element={<Authorized token={token} />}>
+      <Route path="/categories" element={<CategoryList />} />
         {/* Add Routes here */}
         
       </Route>
