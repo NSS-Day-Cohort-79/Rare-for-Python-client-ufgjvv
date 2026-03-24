271e7415ee171ed55ba41b858cee2a3ad8162188
Jack Gardner <jtgardner99@gmail.com>
2026-03-24 15:41:25 -0500

    Able to view current user's posts

--- src/components/posts/UserPosts.js
+++ src/components/posts/UserPosts.js
@@ -13,10 +13,36 @@ import { Link } from "react-router-dom"
 
 export const UserPosts = ({ token }) => {
     const [userPosts, setUserPosts] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         getUserPostsByToken(token).then(setUserPosts)
     }, [token])
+
+    useEffect(() => {
+        if (userPosts.length > 0) {
+            setIsLoading(false)
+        }
+    }, [userPosts])
+
+    const formatDateTime = (dateTime) => {
+        if (!dateTime) return ""
+
+        const date = new Date(dateTime)
+
+        return date.toLocaleString('en-US', {
+            month: 'numeric',
+            day: 'numeric',
+            year: 'numeric',
+            hour: 'numeric',
+            minute: '2-digit',
+            hour12: true
+        })
+    }
+
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
 
     return (
         <div>
@@ -31,10 +57,12 @@ export const UserPosts = ({ token }) => {
                     return (
                         <div>
                             <div>{post.title}</div>
-                            <div>Publication Date: {post.publication_date}</div>
-                            <div>{post.image_url}</div>
-                            <div>Author: {post.user?.first_name} {post.user?.last_name}</div>
-                            <div>Category: {post.category?.label}</div>
+                            <div>Publication Date: {formatDateTime(post.publication_date)}</div>
+                            <div>
+                                <img src={post.image_url} alt="An example for posts" />
+                            </div>
+                            <div>Author: {post.first_name} {post.last_name}</div>
+                            <div>Category: {post.label}</div>
                         </div>
                     )
                 })}
