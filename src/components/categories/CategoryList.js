12b41448457c838b02f3d72beb50c95a425d5771
Evan Alford <evan.al4d@gmail.com>
2026-03-24 10:31:22 -0500

    addded categories component and category manager

---
+++ src/components/categories/CategoryList.js
@@ -0,0 +1,24 @@
+import { useEffect,useState } from "react";
+import { getCategories } from "../../managers/CategoryManager";
+
+export const CategoryList = () => {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        getCategories()
+        .then(data => setCategories(data))
+    }, [])
+
+
+
+    return (
+        <div>
+            <h2>Categories</h2>
+            <ul>
+                {categories.map(category => (
+                    <li key ={category.id}> {category.label}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
