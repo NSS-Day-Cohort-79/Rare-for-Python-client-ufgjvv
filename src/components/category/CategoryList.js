fad5d06e3922545fc530091100fc53e2f609f063
Jordan Haddock <haddockjordan40@gmail.com>
2026-03-25 10:04:50 -0500

    initial commit

--- src/components/category/CategoryList.js
+++ src/components/category/CategoryList.js
@@ -1,24 +1,21 @@
-import { useEffect,useState } from "react";
-import { getCategories } from "../../managers/CategoryManager";
+import { useEffect, useState } from "react";
+import { getCategories } from "../../managers/categoryManager";
 
 export const CategoryList = () => {
-    const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState([]);
 
-    useEffect(() => {
-        getCategories()
-        .then(data => setCategories(data))
-    }, [])
+  useEffect(() => {
+    getCategories().then((data) => setCategories(data));
+  }, []);
 
-
-
-    return (
-        <div>
-            <h2>Categories</h2>
-            <ul>
-                {categories.map(category => (
-                    <li key ={category.id}> {category.label}</li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2>Categories</h2>
+      <ul>
+        {categories.map((category) => (
+          <li key={category.id}> {category.label}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
--- src/managers/categoryManager.js
+++ src/managers/categoryManager.js
@@ -1,3 +1,2 @@
 export const getCategories = () => {
     return fetch("http://localhost:8088/categories").then(res => res.json())
-}
