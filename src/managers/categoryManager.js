6754bb4eff81ad10fae2a69328d647c1feef5b9f
Jack Gardner <jtgardner99@gmail.com>
2026-03-26 10:23:23 -0500

    initial commit

--- src/managers/categoryManager.js
+++ src/managers/categoryManager.js
@@ -2,3 +2,18 @@ export const getCategories = () => {
     return fetch("http://localhost:8088/categories")
     .then(res => res.json())
 }
+
+export const getCategoryById = (id) => {
+    return fetch(`http://localhost:8088/categories?id=${id}`).then(res => res.json())
+}
+
+export const updateCategory = (category) => {
+    return fetch(`http://localhost:8088/categories?id=${category.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(category)
+    })
+}
