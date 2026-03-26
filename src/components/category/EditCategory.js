7415cab2a371a5adcb78d6040dd66fe5eedf6a24
Jack Gardner <jtgardner99@gmail.com>
2026-03-26 12:04:31 -0500

    initial commit

--- src/components/category/EditCategory.js
+++ src/components/category/EditCategory.js
@@ -54,7 +54,7 @@ export const EditCategory = () => {
                 <button type="submit" disabled={submitting}>
                     {submitting ? "Saving..." : "Save"}
                 </button>
-                <button type="button" disabled={submitting}>
+                <button type="button" onClick={() => {navigate("/categories")}} disabled={submitting}>
                     Cancel
                 </button>
                 </div>
